3d5fff51aeb1a82df97f9f08a1186c9d25672e57
Alexey-Gaidai <1123_666@mail.ru>
2023-05-17 10:20:05 +0700

    сделал рабочую версию перехода к входу и регистрации

--- app/src/main/java/com/example/coursework/FragProfile.kt
+++ app/src/main/java/com/example/coursework/FragProfile.kt
@@ -8,9 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintSet
-import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
+
 
 class FragProfile : Fragment() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +29,21 @@ class FragProfile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_frag_profile, container, false)
+        navController = findNavController()
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.nav_host_profile) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+    private fun navigateToLoginFragment() {
+        navController.navigate(R.id.user_login2)
+    }
+    private fun navigateToRegistrationFragment() {
+        navController.navigate(R.id.user_registration2)
     }
 
     companion object {
@@ -35,8 +54,5 @@ class FragProfile : Fragment() {
 
                 }
             }
-    }
-    private fun showRegistration(){
-
     }
 }
--- app/src/main/java/com/example/coursework/buttons_log_reg.kt
+++ app/src/main/java/com/example/coursework/buttons_log_reg.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 
 
 class buttons_log_reg : Fragment() {
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -20,8 +23,23 @@ class buttons_log_reg : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buttons_log_reg, container, false)
+        val view = inflater.inflate(R.layout.fragment_buttons_log_reg, container, false)
+
+        // Получаем ссылку на NavController
+        navController = findNavController()
+
+        // Назначаем слушатели для кнопок регистрации и входа
+        val registrationButton = view.findViewById<Button>(R.id.registration)
+        registrationButton.setOnClickListener {
+            navController.navigate(R.id.user_registration2)
+        }
+
+        val loginButton = view.findViewById<Button>(R.id.login)
+        loginButton.setOnClickListener {
+            navController.navigate(R.id.user_login2)
+        }
+
+        return view
     }
 
     companion object {
